98567f44aa8826387a14d5510f24ec518bf99e7a
Aimen Khakwani and Katy Henning <katy@>
2016-08-17 14:14:04 -0700

    Add conditions for all vowels

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,11 @@
 //ui logic
 $(document).ready(function(){
   $("form").submit(function(event){
-  console.log("hello");
     event.preventDefault();
     var userInput = $("input#english").val().toLowerCase();
     var userArray = userInput.split('');
 
-    if (userArray[0] === "a"){
+    if ((userArray[0] === "a") || (userArray[0] === "e")|| (userArray[0] === "i")|| (userArray[0] === "o")|| (userArray[0] === "u")){
       userArray = userArray + "ay";
       alert(userArray);
     } else {
