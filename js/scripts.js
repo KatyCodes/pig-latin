fa4e3235bf94c1cce1f522b26cd1e627211475ba
Aimen Khakwani and Katy Henning <aimen@>
2016-08-17 16:34:28 -0700

    fiex the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,38 @@
 // business logic
 
+function myFunction (userInput){
+  var vowelArray = ["a", "e", "i", "o", "u"];
+  for (var index = 0; index < userInput.length; index += 1){
+      if (!vowelArray.includes(userInput[0])){
+           userInput.push(userInput.shift());
+           userInput.push("ay");
+           userInput= userInput.join("");
+           return userInput;
+     } else if (vowelArray.includes(userInput[0])){
+         userInput.push("ay");
+         userInput= userInput.join("");
+         return userInput;
+       }
+     }
+}
+
 //ui logic
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = $("input#english").val().toLowerCase();
+    var userInput = $("input#english").val().toLowerCase().split("");
     // var userArray = userInput.split('');
-    var vowelArray = ["a"];
-    debugger;
-    for (var index = 0; index < userInput.length; index += 1){
-      if (!vowelArray.includes(userInput[0])){
-           userInput.push(userInput.shift());
-           $(".output").text(userInput);
-     } else if (vowelArray.includes(userInput[0])){
-         userInput.push("ay");
-         $(".output").text(userInput);
-     }
-  }
 
-
-  if (userInput.charAt[0] === "a"){
-    userInput = userInput + "ay";
-  }
-   else if (userInput.charAt[0]) === "e"{
-     userInput = userInput + "ay";
-   }
-   //...
-   else {
-     return;
-   }
+  // if (userInput.charAt[0] === "a"){
+  //   userInput = userInput + "ay";
+  // }
+  //  else if (userInput.charAt[0]) === "e"{
+  //    userInput = userInput + "ay";
+  //  }
+  //  //...
+  //  else {
+  //    return;
+  //  }
     // if (vowelArray.includes(userInput[0])){
     //     userInput = userInput + "ay";
     //     $(".output").text(userInput);
@@ -38,7 +42,7 @@ $(document).ready(function(){
     //
     //     if (!vowelArray.includes(userInput[1])){
     //         userInput.push(userInput.shift());
-    //         $(".output").text(userInput);
+       $(".output").text(myFunction(userInput));
     //   }
     // }
   });
