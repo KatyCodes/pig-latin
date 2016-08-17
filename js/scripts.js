438a591d8d8d8e074fc0487aef60cb4f9dda3025
Aimen Khakwani and Katy Henning <katy@>
2016-08-17 16:04:37 -0700

    Push first consonant to the back

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,43 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = $("input#english").val().toLowerCase().split("");
+    var userInput = $("input#english").val().toLowerCase();
     // var userArray = userInput.split('');
+    var vowelArray = ["a"];
+    debugger;
+    for (var index = 0; index < userInput.length; index += 1){
+      if (!vowelArray.includes(userInput[0])){
+           userInput.push(userInput.shift());
+           $(".output").text(userInput);
+     } else if (vowelArray.includes(userInput[0])){
+         userInput.push("ay");
+         $(".output").text(userInput);
+     }
+  }
 
 
-    if ((userInput[0] === "a") || (userInput[0] === "e")|| (userInput[0] === "i")|| (userInput[0] === "o")|| (userInput[0] === "u")){
-      userInput = userInput + "ay";
-    } else {
-      userInput.push(userInput.shift());
-
-      alert(userInput);
-    }
+  if (userInput.charAt[0] === "a"){
+    userInput = userInput + "ay";
+  }
+   else if (userInput.charAt[0]) === "e"{
+     userInput = userInput + "ay";
+   }
+   //...
+   else {
+     return;
+   }
+    // if (vowelArray.includes(userInput[0])){
+    //     userInput = userInput + "ay";
+    //     $(".output").text(userInput);
+    // } else {
+    //     userInput.push(userInput.shift());
+    //     $(".output").text(userInput);
+    //
+    //     if (!vowelArray.includes(userInput[1])){
+    //         userInput.push(userInput.shift());
+    //         $(".output").text(userInput);
+    //   }
+    // }
   });
 });
 
